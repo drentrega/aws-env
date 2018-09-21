02a7094108d9d9266f3d6255937f2ddd8dfefa68
Filipe Forattini <filipeforattini1@gmail.com>
2018-09-21 00:02:30 -0300

    build(*): merge (#3)
    * build(package): forcing dre namespace version
    
    * build(*): dre version
    
    * build(*): dre version
    
    * fix(*): prefix on versioning
    
    * build(*): dre version
    
    * fix(*): removing releases from package
    
    * fix(*): dependencies
    
    * fix(*): tags
    
    * fix(*): default null breaking command line
    
    * build(package-lock): new lock
    
    * fix(*): removed console logs
    
    * refactor(*): name
    
    * refactor(*): everything to aws-env
    
    * docs(*): adding credits
    
    * feat(*): organizing and added verbose option
    
    * docs(*): new parameters and options
    
    * build(*): update to releases
    
    * build(*): new version packaging
    
    * fix(*): cases when prefix might break
    
    * fix(*): cases when prefix might break
    
    * build(*): new release

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const AwsSsm = require('./vendor/aws-ssm');
 const { DEFAULT_ERROR_MSG } = require('./concerns/msgs');
 
 module.exports = async (params) => {
-  if (params.verbose) console.debug('DEBUG |', JSON.stringify({ params }));
+  if (params.verbose) console.debug('DEBUG | input', JSON.stringify({ params }));
 
   if (!params.namespace) {
     console.error(DEFAULT_ERROR_MSG);
     process.exit(1);
   }
+
+  if (typeof params.prefix === 'boolean') params.prefix = '';
 
   let response = null;
   try {
